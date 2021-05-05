1418e1393e70d7fcc2a0ef868581c886775b0d01
SeongEunPark <sungeun1214@gmail.com>
2021-05-05 18:03:42 +0900

    Implement volume-controller of VideoPlayer component

--- components/video/VideoPlayer.js
+++ components/video/VideoPlayer.js
@@ -9,42 +9,102 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react';
 import { useRef } from 'react';
 import PauseIcon from '@material-ui/icons/Pause';
+import classNames from 'classnames';
 
 const iconStyle = {color: '#fff'}
 
 const VideoPlayer = (props) => {
   const {
-    miniPlayer
+    volume
   } = props
 
   // Controller
   const videoPlayerRef = useRef()
-  const [ isVideoPlaying, setIsVideoPlaying ] = useState(false)
+  const [ isVideoPlaying, setIsVideoPlaying ] = useState(false) // Video state
+  const [ videoVolume, setVideoVolume ] = useState(volume ? volume : 25) // Video state
 
-  useEffect(() => {
-    console.log('videoPlayerRef.current :>>', videoPlayerRef.current)
-    // const { controller } = videoPlayerRef.current
-    // console.log('controller :>> ', controller);
-  }, [videoPlayerRef])
+  // UI states
+  const [ isVolumeHover, setIsVolumeHover ] = useState(false)
 
-  const onClickVideoPlayBtn = (e) => {
+  // useEffect(() => {
+  //   console.log('videoPlayerRef.current :>>', videoPlayerRef.current)
+  // }, [videoPlayerRef])
+  
+  /**
+   * Controll state
+   */
+  // Toggle video play/pause
+  const toggleVideoPlayPause = () => {
+    // Toggle play/pause state
     if (!isVideoPlaying) {
       videoPlayerRef.current.play()
     } else {
       videoPlayerRef.current.pause()
     }
-
     setIsVideoPlaying(!isVideoPlaying)
   }
-   
+  // Control video volume
+  const controlVideoVolume = (value) => {
+    value = value / 100
+    videoPlayerRef.current.volume = value
+  }
+
+  /**
+   * Video
+   * Video frame clicked
+   * @param {*} e 
+   */
+  const onClickVideo = (e) => {
+    toggleVideoPlayPause()
+  }
+  // On video played
+  const onVideoPlay = (e) => {
+    setIsVideoPlaying(true)
+  }
+  // On video ended
+  const onVideoEnded = (e) => {
+    setIsVideoPlaying(false)
+  }
+
+  /**
+   * VideoController
+   */
+  // Play/Pause button clicked
+  const onClickVideoPlayBtn = (e) => {
+    toggleVideoPlayPause()
+  }
+  // Volume controller hovered
+  const onHoverVideoVolumeBtn = (e) => {
+    setIsVolumeHover(true)
+  }
+  // Volume controller mouse leaved
+  const onLeaveVideoVolumeBtn = (e) => {
+    setIsVolumeHover(false)
+  }
+  // Sound slider value changed
+  const onChangeVideoSound = (e) => {
+    const { value } = e.target
+    setVideoVolume(value)
+    controlVideoVolume(value)
+  }
+
+  // Play/Pause button clicked
   const onClickSmallPlayerBtn = (e) => {
     props.setGlobalVideo(true)
   }
 
+  // Volume controller
+  const volumeControllerClassnames = classNames('video-volume-controller', {
+    'hovered': isVolumeHover
+  })
+
   return (
     <>
       <div className="yt-video-player">
-        <video className="video-viewer" ref={videoPlayerRef}>
+        <video className="video-viewer" ref={videoPlayerRef} volume={videoVolume} autoPlay={true}
+          onClick={onClickVideo} 
+          onPlay={onVideoPlay}
+          onEnded={onVideoEnded}>
           <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
         </video>
     
@@ -65,15 +125,20 @@ const VideoPlayer = (props) => {
                 <button className="video-next">
                   <SkipNextIcon style={iconStyle} />
                 </button>
-                <button className="video-volume-btn">
-                  <VolumeUpIcon style={iconStyle} />
-                </button>
+                <div className={volumeControllerClassnames} onMouseEnter={onHoverVideoVolumeBtn} onMouseLeave={onLeaveVideoVolumeBtn}>
+                  <button className="video-volume-btn">
+                    <VolumeUpIcon style={iconStyle} />
+                  </button>
+                  <div className="video-volume-slider">
+                    <input type="range" value={videoVolume} min="0" max="100" onChange={onChangeVideoSound} />
+                  </div>
+                </div>
                 <div className="video-time">
                   <span className="video-current-point">0:00</span> / <span className="video-end-point">14:00</span>
                 </div>
-              </div>
-              <div id="middle" className="video-timeline">
-                
+                <div className="video-timeline">
+
+                </div>
               </div>
               <div id="end">
                 <Link href={"/"}>
@@ -119,7 +184,7 @@ const VideoPlayer = (props) => {
         .yt-video-player .video-controller .controller-grid {
           display: grid;
           height: calc(100% - 4px);
-          grid-template-columns: 200px auto 194px;
+          grid-template-columns: auto 194px;
         }
         .yt-video-player .video-controller .controller-grid > div > * {
           display: inline-block;
@@ -148,8 +213,40 @@ const VideoPlayer = (props) => {
           height: 100%;
           background-color: #cc0000;
         }
+        .yt-video-player .video-controller .video-volume-controller {
+          position: relative;
+          width: 44px;
+          height: 40px;
+          transition: width 0.6s;
+        }
+        .yt-video-player .video-controller .video-volume-controller.hovered {
+          width: 128px;
+        }
+        .yt-video-player .video-controller .video-volume-controller .video-volume-slider {
+          position: absolute;
+          top: 0px;
+          left: 44px;
+          width: 0px;
+          opacity: 0;
+          margin-left: 4px;
+          transition: width 0.6s, opacity 0.4s;
+        }
+        .yt-video-player .video-controller .video-volume-controller.hovered .video-volume-slider {
+          width: 80px;
+          opacity: 1;
+        }
+        .yt-video-player .video-controller .video-volume-controller .video-volume-slider input {
+          width: 0px;
+          line-height: 40px;
+          margin: 0;
+          transition: width 0.6s;
+        }
+        .yt-video-player .video-controller .video-volume-controller.hovered .video-volume-slider input {
+          width: 80px;
+        }
         .yt-video-player .video-controller .video-time {
           font-size: 0.8rem;
+          padding-left: 4px;
         }
         .yt-video-player .video-controller button {
           padding: 8px 10px 5px;
